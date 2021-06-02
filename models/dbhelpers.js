928f759c4dfc4ee08acc77d0ee11eeaa58413a79
comfybear71 <stuart@psal.net.au>
2021-06-02 21:19:55 +0930

    Corrected insert function for pg

--- models/dbhelpers.js
+++ models/dbhelpers.js
@@ -18,8 +18,9 @@ module.exports = {
 }
 
 async function add(lesson) {
-    const [id] = await db('lessons').insert(lesson);
-    return id
+    return await db('lessons').insert(lesson, ['id', 'name'])
+    // const [id] = await db('lessons').insert(lesson);
+    // return findById(id)
 }
 
 function find() {
@@ -54,10 +55,13 @@ function findMessageById(id) {
 }
 
 async function addMessage(message, lesson_id) {
-    const [id] = await db("messages")
+    return await db('messages')
     .where({ lesson_id })
-    .insert(message)
-    return findMessageById(id)
+    .insert(message, ['id'])
+    // const [id] = await db("messages")
+    // .where({ lesson_id })
+    // .insert(message)
+    // return findMessageById(id)
 }
 
 function findLessonMessages(lesson_id) {
